70f25c6f214bef19f89a32f50d4be2cbee4f58d3
harsh <harsh123@gmail.com>
2023-08-29 17:37:09 +0530

    solved comflict

--- components/layout.tsx
+++ components/layout.tsx
@@ -376,7 +376,7 @@ export default function Layout(props: LayoutProps) {
           <div className="flex justify-center items-center bg-[#00000080] overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t items-center">
+                <div className="flex  justify-between p-5 border-b border-solid border-gray-300 rounded-t items-center">
                   <h5 className="text-3xl font=semibold">Change Password</h5>
                   <button
                     className="bg-transparent border-0 text-black float-right"
