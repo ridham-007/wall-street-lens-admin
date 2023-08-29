a7f06faac76df3810f381e7204a92cbf7c8bc49d
Ridham Avaiya <ridhamavaiya1999@gmail.com>
2023-08-29 16:06:30 +0530

    Integrate perameters query

--- utils/query.ts
+++ utils/query.ts
@@ -17,6 +17,30 @@ mutation createFinancialSummary($summaryInfo:CreateFinancialSummary!){
   }
 }`;
 
+const GET_FINANCIAL_SUMMARY_PARAMETERS = gql`
+query getFinancialSummaryByCompany($companyName: String!){
+  getFinancialSummaryByCompany(
+    companyName: $companyName
+  ) {
+    id
+    company
+    linkTo
+    isVisibleToChart
+    unit
+  }
+}`;
+
+const ADD_QUARTERS_DETAILS = gql`
+  mutation addQuarters($addQuarters: AddQuarters!){
+  addQuarters(addQuarters: $addQuarters) {
+    success
+    affectedRow
+  }
+}
+`;
+
 export {
     ADD_FINANCIAL_SUMMARY_PARAMETER,
+    GET_FINANCIAL_SUMMARY_PARAMETERS,
+    ADD_QUARTERS_DETAILS,
 }
