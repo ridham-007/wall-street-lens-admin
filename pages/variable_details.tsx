490b9828c1b9881049f449219b2af702b4536137
Urvashi <ridhamavaiya1999@gmail.com>
2023-09-15 04:17:31 -0700

    Delete Quarter button create

--- pages/variable_details.tsx
+++ pages/variable_details.tsx
@@ -85,7 +85,8 @@ export default function VariableDetails() {
   return (
     <Layout title="Financial Summary" page={LayoutPages.variable_details}>
       <>
-        <div className="flex gap-[20px]">
+        <div className="flex justify-between gap-[20px]">
+          <div className="flex  gap-[20px] ">
           <div className="flex flex-col mb-[20px]">
             <label htmlFor="quarter" className="text-sm font-bold text-gray-700">
               KPIs Term:
@@ -107,7 +108,7 @@ export default function VariableDetails() {
                 );
               })}
             </select>
-          </div>
+            </div>
           {selectedTerm?.quarterWiseTable && (<><div className="flex flex-row items-center gap-[20px] mb-[20px]">
             <YearDropdown onChange={(event: { target: { value: SetStateAction<string>; }; }) => { setYear(event?.target.value)}} year={year}/>
           </div>
@@ -129,8 +130,11 @@ export default function VariableDetails() {
               <option value="3">3</option>
               <option value="4">4</option>
             </select>
-            </div></>)}
-
+            </div>
+            </>
+            )}
+          </div> 
+          <div>
           <button
             type="button"
             className="bg-blue-500 hover:bg-blue-600 transform hover:scale-105 text-white font-medium rounded-lg py-3 px-3 inline-flex items-center space-x-2 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 ml-auto h-[50px]"
@@ -158,6 +162,17 @@ export default function VariableDetails() {
             </svg>
             <span>Add a Quarter</span>
           </button>
+          {selectedTerm?.quarterWiseTable && (
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-600 transform hover:scale-105 text-white font-medium rounded-lg ml-3 py-3 px-3 inline-flex items-center space-x-2 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 ml-auto h-[50px]"
+            
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 48 48" width="28px" height="28px">
+                  <path d="M 24 4 C 20.491685 4 17.570396 6.6214322 17.080078 10 L 10.238281 10 A 1.50015 1.50015 0 0 0 9.9804688 9.9785156 A 1.50015 1.50015 0 0 0 9.7578125 10 L 6.5 10 A 1.50015 1.50015 0 1 0 6.5 13 L 8.6386719 13 L 11.15625 39.029297 C 11.427329 41.835926 13.811782 44 16.630859 44 L 31.367188 44 C 34.186411 44 36.570826 41.836168 36.841797 39.029297 L 39.361328 13 L 41.5 13 A 1.50015 1.50015 0 1 0 41.5 10 L 38.244141 10 A 1.50015 1.50015 0 0 0 37.763672 10 L 30.919922 10 C 30.429604 6.6214322 27.508315 4 24 4 z M 24 7 C 25.879156 7 27.420767 8.2681608 27.861328 10 L 20.138672 10 C 20.579233 8.2681608 22.120844 7 24 7 z M 11.650391 13 L 36.347656 13 L 33.855469 38.740234 C 33.730439 40.035363 32.667963 41 31.367188 41 L 16.630859 41 C 15.331937 41 14.267499 40.033606 14.142578 38.740234 L 11.650391 13 z M 20.476562 17.978516 A 1.50015 1.50015 0 0 0 19 19.5 L 19 34.5 A 1.50015 1.50015 0 1 0 22 34.5 L 22 19.5 A 1.50015 1.50015 0 0 0 20.476562 17.978516 z M 27.476562 17.978516 A 1.50015 1.50015 0 0 0 26 19.5 L 26 34.5 A 1.50015 1.50015 0 1 0 29 34.5 L 29 19.5 A 1.50015 1.50015 0 0 0 27.476562 17.978516 z"/></svg>
+            <span>Delete Quarter</span>
+          </button>)}
+          </div>
         </div>
         {!!termId && <Variable termId={termId} year={year} quarter={quarter} selectedTerm={selectedTerm}/>}
         {showQuarter && (<QuarterData onSuccess={handleOnAddQuarter} onClose={() => {setShowQuarter(false)}}/>)}
