223385bbc285b9e197f0e31fab51de5ab1e6c4ce
Urvashi <ridhamavaiya1999@gmail.com>
2023-09-21 21:20:42 -0700

    Fix Edit popups

--- components/table/variables/Variable.tsx
+++ components/table/variables/Variable.tsx
@@ -12,8 +12,10 @@ export interface TableProps {
   year: string;
   quarter: string;
   setShowDelete: any;
+  setShowEdit: any;
   setDeleteId: any;
   refetch: boolean;
+  setEditId: any;
 }
 
 export interface Header {
@@ -45,7 +47,9 @@ export default function Variable({
   year,
   quarter,
   setShowDelete,
+  setShowEdit,
   setDeleteId,
+  setEditId
 }: TableProps) {
   const [getTermView, { data: termView, refetch: refetchTermView }] =
     useLazyQuery(GET_VIEW_FOR_TERM, {
@@ -78,7 +82,6 @@ export default function Variable({
   const checkValidId = (id: string) => {
     return !!id && !id.startsWith("Dummy");
   };
-
   const onSave = async (
     id: string,
     value: string,
@@ -116,6 +119,12 @@ export default function Variable({
     setDeleteId(identifier);
   };
 
+  const handleShowEdit = (identifier: any) => {
+    setShowEdit(true);
+    setEditId(identifier);
+    
+  };
+  
   const selectedColumn = headers[cellData?.columnIndex];
 
   return (
@@ -157,6 +166,15 @@ export default function Variable({
                               >
                                 <path d="M 24 4 C 20.491685 4 17.570396 6.6214322 17.080078 10 L 10.238281 10 A 1.50015 1.50015 0 0 0 9.9804688 9.9785156 A 1.50015 1.50015 0 0 0 9.7578125 10 L 6.5 10 A 1.50015 1.50015 0 1 0 6.5 13 L 8.6386719 13 L 11.15625 39.029297 C 11.427329 41.835926 13.811782 44 16.630859 44 L 31.367188 44 C 34.186411 44 36.570826 41.836168 36.841797 39.029297 L 39.361328 13 L 41.5 13 A 1.50015 1.50015 0 1 0 41.5 10 L 38.244141 10 A 1.50015 1.50015 0 0 0 37.763672 10 L 30.919922 10 C 30.429604 6.6214322 27.508315 4 24 4 z M 24 7 C 25.879156 7 27.420767 8.2681608 27.861328 10 L 20.138672 10 C 20.579233 8.2681608 22.120844 7 24 7 z M 11.650391 13 L 36.347656 13 L 33.855469 38.740234 C 33.730439 40.035363 32.667963 41 31.367188 41 L 16.630859 41 C 15.331937 41 14.267499 40.033606 14.142578 38.740234 L 11.650391 13 z M 20.476562 17.978516 A 1.50015 1.50015 0 0 0 19 19.5 L 19 34.5 A 1.50015 1.50015 0 1 0 22 34.5 L 22 19.5 A 1.50015 1.50015 0 0 0 20.476562 17.978516 z M 27.476562 17.978516 A 1.50015 1.50015 0 0 0 26 19.5 L 26 34.5 A 1.50015 1.50015 0 1 0 29 34.5 L 29 19.5 A 1.50015 1.50015 0 0 0 27.476562 17.978516 z" />
                               </svg>
+                            </span>
+                          )}{!selectedTerm?.quarterWiseTable && (
+                            <span
+                              className="px-2 cursor-pointer"
+                              onClick={() => {
+                                handleShowEdit(current.id);
+                              }}
+                            >
+                             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="24px" height="24px"><path d="M 23.90625 3.96875 C 22.859375 3.96875 21.8125 4.375 21 5.1875 L 5.1875 21 L 5.125 21.3125 L 4.03125 26.8125 L 3.71875 28.28125 L 5.1875 27.96875 L 10.6875 26.875 L 11 26.8125 L 26.8125 11 C 28.4375 9.375 28.4375 6.8125 26.8125 5.1875 C 26 4.375 24.953125 3.96875 23.90625 3.96875 Z M 23.90625 5.875 C 24.410156 5.875 24.917969 6.105469 25.40625 6.59375 C 26.378906 7.566406 26.378906 8.621094 25.40625 9.59375 L 24.6875 10.28125 L 21.71875 7.3125 L 22.40625 6.59375 C 22.894531 6.105469 23.402344 5.875 23.90625 5.875 Z M 20.3125 8.71875 L 23.28125 11.6875 L 11.1875 23.78125 C 10.53125 22.5 9.5 21.46875 8.21875 20.8125 Z M 6.9375 22.4375 C 8.136719 22.921875 9.078125 23.863281 9.5625 25.0625 L 6.28125 25.71875 Z"/></svg>
                             </span>
                           )}
                         </div>
